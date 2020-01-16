50841d513464277b5d5a28389db1138b37e24575
NimSum <nimm.garcia@gmail.com>
2020-01-16 10:48:03 -0600

    Create Certificate of Deposit model

---
+++ Calculator/Models/CertificateOfDeposit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+
+    public class CertificateOfDeposit
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public int AccountNumber { get; set; }
+        public string Type { get; set; }
+        public string Term { get; set; }
+        public decimal InterestRate { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
