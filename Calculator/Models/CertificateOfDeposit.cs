95a6fa40e274b7788d2a1d31986b28490357b79b
NimSum <nimm.garcia@gmail.com>
2020-01-16 12:23:26 -0600

    Update table data annotations and term data type

--- Calculator/Models/CertificateOfDeposit.cs
+++ Calculator/Models/CertificateOfDeposit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,11 @@ namespace Calculator.Models
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public int AccountNumber { get; set; }
         public string Type { get; set; }
-        public string Term { get; set; }
+        public int Term { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal InterestRate { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
+        public string PortfolioId { get; set; }
     }
+
 }
--- Calculator/Models/CreditCard.cs
+++ Calculator/Models/CreditCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,15 @@ namespace Calculator.Models
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public int AccountNumber { get; set; }
         public string Type { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Balance { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal InterestRate { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Income { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal UsageByPercentage { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
+        public string PortfolioId { get; set; }
     }
 }
