b22caa4cbd3e9f46175683da0f5f149ed6849538
NimSum <nimm.garcia@gmail.com>
2020-01-20 10:20:38 -0600

    Create REST API and CRUD functionality

---
+++ Calculator/Controllers/CreditCardController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calculator.Controllers
+{
+    public class CreditCardController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public CreditCardController(AppDBContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index(string id)
+        {
+            if (id != null)
+            {
+                return RedirectToAction("Details", new { id });
+            };
+
+            return Ok(_context.CreditCards);
+        }
+
+        public IActionResult Details(string id)
+        {
+            if (id != null)
+            {
+                return Ok(_context.CreditCards.Find(id));
+            };
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CreditCard model)
+        {
+            if (ModelState.IsValid)
+            {
+                CreditCard creditCard = new CreditCard
+                {
+                    AccountNumber = model.AccountNumber,
+                    Type = model.Type,
+                    Balance = model.Balance,
+                    InterestRate = model.InterestRate,
+                    Income = model.Income,
+                    UsageByPercentage = model.UsageByPercentage,
+                    PortfolioId = model.PortfolioId
+                };
+
+                _context.CreditCards.Add(creditCard);
+                _context.SaveChanges();
+
+                return Ok(creditCard);
+            }
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromBody]CreditCard model)
+        {
+            if (ModelState.IsValid)
+            {
+                CreditCard creditCard = _context.CreditCards.Find(model.Id);
+
+                if (creditCard != null)
+                {
+                    creditCard.AccountNumber = model.AccountNumber;
+                    creditCard.Type = model.Type;
+                    creditCard.Balance = model.Balance;
+                    creditCard.InterestRate = model.InterestRate;
+                    creditCard.Income = model.Income;
+                    creditCard.UsageByPercentage = model.UsageByPercentage;
+                    creditCard.PortfolioId = model.PortfolioId;
+
+                    var creditCardUpdate = _context.CreditCards.Attach(creditCard);
+                    creditCardUpdate.State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    return Ok(creditCard);
+                }
+            }
+
+            return Ok(model);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            if (id != null)
+            {
+                CreditCard creditCard = _context.CreditCards.Find(id);
+                _context.CreditCards.Remove(creditCard);
+                _context.SaveChanges();
+
+                return Ok(creditCard);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Calculator/Controllers/PortfolioController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using Calculator.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calculator.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly AppDBContext _context;
+
+
+        public PortfolioController(AppDBContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index(string id)
+        {
+            if (id != null)
+            {
+                return RedirectToAction("Details", new { id });
+            };
+
+            return Ok(_context.Portfolios);
+        }
+
+        public IActionResult Details(string id)
+        {
+            if (id != null)
+            {
+                Portfolio portfolio = _context.Portfolios
+                    .Include(p => p.Loans)
+                    .Include(p => p.CertificateOfDeposits)
+                    .Include(p => p.CreditCards)
+                    .Include(p => p.Deposits)
+                    .FirstOrDefault(p => p.Id == id);
+
+                return Ok(portfolio);
+            };
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Portfolio model)
+        {
+            if (ModelState.IsValid)
+            {
+                Portfolio portfolio = new Portfolio
+                {
+                    Name = model.Name,
+                    Type = model.Type
+                };
+
+                _context.Portfolios.Add(portfolio);
+                _context.SaveChanges();
+
+                return Ok(portfolio);
+            }
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromBody]Portfolio model)
+        {
+            if (ModelState.IsValid)
+            {
+                Portfolio portfolio = _context.Portfolios.Find(model.Id);
+
+                if (portfolio != null)
+                {
+                    portfolio.Name = model.Name;
+                    portfolio.Type = model.Type;
+
+                    var portfolioUpdate = _context.Portfolios.Attach(portfolio);
+                    portfolioUpdate.State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    return Ok(portfolio);
+                }
+            }
+
+            return Ok(model);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            if (id != null)
+            {
+                Portfolio portfolio = _context.Portfolios.Find(id);
+                _context.Portfolios.Remove(portfolio);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
