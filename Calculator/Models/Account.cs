c97b9c7d169497778f3c7897f9fcf4291fc12c89
NimSum <nimm.garcia@gmail.com>
2020-01-20 13:00:41 -0600

    Create Account model

---
+++ Calculator/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class Account
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required]
+        public int AccountNumber { get; set; }
+        [Required]
+        [ForeignKey("Portfolio")]
+        public string PortfolioId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
